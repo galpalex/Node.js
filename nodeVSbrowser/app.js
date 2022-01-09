066ca2cacd375894b2d83eeefa22fe0f749b3edf
galpalex <galpalex@gmail.com>
2022-01-09 17:10:55 +0200

    node vs browser

---
+++ nodeVSbrowser/app.js
@@ -0,0 +1,12 @@
+// 3 things node.js has that the client side javascript doesn’t
+// have:
+// 1. can be used as general programming language, to build web servers
+// 2. not limited to operations that browser allow to do
+// 3. a way to run js code on server, it interacts with filesystem
+// 4. has global method istead of window in browser, has process instead of document
+
+// 3 things the client side javascript has that node.js doesn’t
+// have:
+// 1. can run on browsers
+// 2. can manipulate dom, add event listener to buttons
+// 3. has window method, document
---
+++ nodeVSbrowser/readme.md
@@ -0,0 +1,10 @@
+Backend: Node JS
+Node JS – node vs js
+The following exercise contains the following subjects:
+ Node JS
+Instructions
+Write in your own words the following:
+What are 3 things node.js has that the client side javascript doesn’t
+have. And the reasons why.
+What are 3 things the client side javascript has that node.js doesn’t
+have. And the reasons why.
