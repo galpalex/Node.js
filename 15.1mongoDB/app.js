f813edf3fcef1f891b58af8ddd22eb2b62164d59
galpalex <galpalex@gmail.com>
2022-01-17 19:20:27 +0200

    15.1 init mongo

---
+++ 15.1mongoDB/app.js
@@ -0,0 +1,62 @@
+// [
+//   ({
+//     _id: ObjectId("61e551958bef5ff9005d77ab"),
+//     name: "kfc",
+//     address: {
+//       city: "Tel Aviv",
+//       street: "Herzl",
+//       coordinates: [-77.444, 50.432],
+//     },
+//     type: "American",
+//     kosher: false,
+//     reviews: [{ date: "1-1-22", score: 3 }],
+//   },
+//   {
+//     _id: ObjectId("61e552818bef5ff9005d77ac"),
+//     name: "Chuchi",
+//     address: {
+//       city: "Herzelia",
+//       street: "Ben Gurion",
+//       coordinates: [-74.554, 50.452],
+//     },
+//     type: "Asian",
+//     kosher: false,
+//     reviews: [{ date: "1-1-21", score: 4 }],
+//   },
+//   {
+//     _id: ObjectId("61e553478bef5ff9005d77ad"),
+//     name: "Mia",
+//     address: {
+//       city: "Nesziona",
+//       street: "Sireni",
+//       coordinates: [-71.444, 42.432],
+//     },
+//     type: "Mexican",
+//     kosher: false,
+//     reviews: [{ date: "10-1-22", score: 5 }],
+//   },
+//   {
+//     _id: ObjectId("61e555a28bef5ff9005d77ae"),
+//     name: "Temple",
+//     address: {
+//       city: "Ramat Gan",
+//       street: "Ehad",
+//       coordinates: [-71.144, 22.432],
+//     },
+//     type: "Italian",
+//     kosher: true,
+//     reviews: [{ date: "5-1-22", score: 4 }],
+//   },
+//   {
+//     _id: ObjectId("61e556208bef5ff9005d77af"),
+//     name: "Matuka",
+//     address: {
+//       city: "Rishon-lezion",
+//       street: "Hagana",
+//       coordinates: [-71.144, 22.432],
+//     },
+//     type: "Italian",
+//     kosher: true,
+//     reviews: [{ date: "5-1-12", score: 2 }],
+//   })
+// ];
---
+++ 15.1mongoDB/readme.md
@@ -0,0 +1,24 @@
+Backend: MongoDB
+Node JS – Restaurant collection
+The following exercise contains the following subjects:
+ mongoDB
+Instructions
+
+1. Create a database called findMyRestaurant
+2. Create a collection called restaurants
+3. inside the collection create at least 5 restaurant
+   documents that include the following:
+    restaurant name
+    a sub document called address that includes:
+   • a city
+   • a street
+   • An array of coordinates e.g. [-77,564, 40.677]
+    a type of cuisine
+    a boolean if it kosher or not
+    An array of at least 3 reviews that include:
+   • a date
+   • an integer score
+   To test your data against our queries do the following:
+   Only 3 different city names
+   Only 3 different types of cuisine
+   Give different dates for your reviews.
